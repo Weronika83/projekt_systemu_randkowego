7116315c9d156f9630208ca395311295f72d7039
Weronika83 <weronika.szelag@student.wat.edu.pl>
2026-06-07 20:39:39 +0200

    wip: mvc template applied

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from controller import pokaz_liste
 
 root = Tk()
 root.title("Portal randkowy")
@@ -6,7 +7,7 @@ root.geometry("1100x760")
 
 Label(root, text="Wybierz moduł").pack()
 
-Button(root, text="Klienci").pack()
+Button(root, text="Klienci", command=pokaz_liste).pack()
 Button(root, text="Pracownicy").pack()
 
 root.mainloop()
