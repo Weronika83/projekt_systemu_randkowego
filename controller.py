76da5b6b14ad3b573e3ab2273a5ea6cc8ca869cf
Weronika83 <weronika.szelag@student.wat.edu.pl>
2026-06-07 23:21:28 +0200

    feat: add update function

--- controller.py
+++ controller.py
@@ -16,12 +16,12 @@ def pokaz_liste():
 
 #----------------DODAWANIE, USUWANIE, EDYTOWANIE----------------
 def dodaj_obiekt(imie, wiek, opis, lokalizacja):
-    nowy_klient = klient(
-        imie,
-        int(wiek),
-        opis,
-        lokalizacja,
-        "klient"
-    )
+    nowy_klient = klient( imie, int(wiek), opis, lokalizacja, "klient")
 
     klienci.append(nowy_klient)
+
+def edytuj_obiekt(index, nowe_imie, nowy_wiek, nowy_opis, nowa_lokalizacja):
+    klienci[index].imie = nowe_imie
+    klienci[index].wiek = int(nowy_wiek)
+    klienci[index].opis = nowy_opis
+    klienci[index].lokalizacja = nowa_lokalizacja
