189ad4474bdf51a292638de7c2d7fb5213c1141e
Weronika83 <weronika.szelag@student.wat.edu.pl>
2026-06-07 16:32:48 +0200

    wip: complex data structure

--- model.py
+++ model.py
@@ -2,6 +2,23 @@ klienci = []
 pracownicy = []
 randki = []
 
-class klient: pass
-class pracownik: pass
-class randka: pass
+class klient:
+    def __init__(self, imie, wiek, opis, lokalizacja, typ):
+        self.imie = imie
+        self.wiek = wiek
+        self.opis = opis
+        self.lokalizacja = lokalizacja
+        self.typ = typ
+
+class pracownik:
+    def __init__(self, imie, wiek, stanowisko, lokalizacja):
+        self.imie = imie
+        self.wiek = wiek
+        self.stanowisko = stanowisko
+        self.lokalizacja = lokalizacja
+
+class randka:
+    def __init__(self, klient, osoba, data):
+        self.klient = klient
+        self.osoba = osoba
+        self.data = data
