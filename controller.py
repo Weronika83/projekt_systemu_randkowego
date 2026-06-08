02be5e0e7b00bf5bb9f5ff24eb8db65bea229643
Weronika83 <weronika.szelag@student.wat.edu.pl>
2026-06-08 13:17:16 +0200

    feat: add map integration

--- controller.py
+++ controller.py
@@ -18,6 +18,7 @@ entry_klient_randka = None
 entry_osoba_randka = None
 entry_data_randka = None
 button_dodaj = None
+map_widget = None
 
 aktualna_lista = klienci
 aktualny_typ = "klient"
@@ -34,6 +35,7 @@ def wybierz_klientow():
     label_opis.config(text="Opis zainteresowań:")
 
     pokaz_liste()
+    wyczysc_formularz()
 
 def wybierz_pracownikow():
     global aktualna_lista, aktualny_typ, edytowany_indeks
@@ -44,6 +46,7 @@ def wybierz_pracownikow():
     label_opis.config(text="Stanowisko:")
 
     pokaz_liste()
+    wyczysc_formularz()
 
 # -------------------- LISTA --------------------
 def pokaz_liste() -> None:
@@ -64,46 +67,41 @@ def dodaj_obiekt():
         messagebox.showwarning("Brak danych", "Uzupełnij wymagane pola")
         return
     try:
-        if aktualny_typ == "klient": nowy_obiekt = klient(imie=imie, wiek=int(wiek), opis=opis, lokalizacja=lokalizacja, typ=aktualny_typ)
+        if aktualny_typ == "klient":
+            nowy_obiekt = klient(imie=imie, wiek=int(wiek), opis=opis, lokalizacja=lokalizacja, typ=aktualny_typ)
         else:
             nowy_obiekt = pracownik(imie=imie, wiek=int(wiek), stanowisko=opis, lokalizacja=lokalizacja)
 
         aktualna_lista.append(nowy_obiekt)
-        pokaz_liste()
+        nowy_obiekt.marker = map_widget.set_marker( nowy_obiekt.coordinates[0], nowy_obiekt.coordinates[1], text=nowy_obiekt.imie)
 
+        pokaz_liste()
+        wyczysc_formularz()
     except ValueError:
         messagebox.showerror("Błąd", "Nie udało się dodać obiektu")
 
-
 def usun_obiekt() -> None:
     zaznaczenie = listbox_klienci.curselection()
-
     if not zaznaczenie:
         messagebox.showwarning("Brak wyboru", "Zaznacz obiekt na liście")
         return
 
     i = listbox_klienci.index(ACTIVE)
+    if aktualna_lista[i].marker:
+        aktualna_lista[i].marker.delete()
     aktualna_lista.pop(i)
     pokaz_liste()
 
 def edytuj_obiekt():
     global edytowany_indeks
-
     zaznaczenie = listbox_klienci.curselection()
-
     if not zaznaczenie:
         messagebox.showwarning("Brak wyboru", "Zaznacz obiekt na liście")
         return
-
     i = listbox_klienci.index(ACTIVE)
     edytowany_indeks = i
-
     obiekt = aktualna_lista[i]
-
-    entry_imie.delete(0, END)
-    entry_wiek.delete(0, END)
-    entry_opis.delete(0, END)
-    entry_lokalizacja.delete(0, END)
+    wyczysc_formularz()
 
     entry_imie.insert(0, obiekt.imie)
     entry_wiek.insert(0, obiekt.wiek)
@@ -114,24 +112,50 @@ def edytuj_obiekt():
     else:
         entry_opis.insert(0, obiekt.stanowisko)
 
+    button_dodaj.config(text="Zapisz zmiany", command=zapisz_zmiany)
+
+def zapisz_zmiany():
+    global edytowany_indeks
+    if edytowany_indeks is None: return
+    i = edytowany_indeks
+    aktualna_lista[i].imie = entry_imie.get()
+    aktualna_lista[i].wiek = int(entry_wiek.get())
+    aktualna_lista[i].lokalizacja = entry_lokalizacja.get()
+    if aktualny_typ == "klient":
+        aktualna_lista[i].opis = entry_opis.get()
+    else:
+        aktualna_lista[i].stanowisko = entry_opis.get()
+
+    if aktualna_lista[i].marker:
+        aktualna_lista[i].marker.delete()
+
+    aktualna_lista[i].marker = map_widget.set_marker(aktualna_lista[i].coordinates[0], aktualna_lista[i].coordinates[1], text=aktualna_lista[i].imie)
+
+    button_dodaj.config(text="Dodaj", command=dodaj_obiekt)
+    edytowany_indeks = None
+
+    wyczysc_formularz()
+    pokaz_liste()
+
 def pokaz_szczegoly():
-    zaznaczenie = listbox_klienci.curselection()
-
-    if not zaznaczenie:
-        messagebox.showwarning("Brak wyboru", "Zaznacz obiekt na liście")
+    if not listbox_klienci.curselection():
         return
-
     i = listbox_klienci.index(ACTIVE)
-    obiekt = aktualna_lista[i]
+    imie = aktualna_lista[i].imie
+    wiek = aktualna_lista[i].wiek
+    lokalizacja = aktualna_lista[i].lokalizacja
 
-    label_imie_wartosc.config(text=obiekt.imie)
-    label_wiek_wartosc.config(text=obiekt.wiek)
-    label_lokalizacja_wartosc.config(text=obiekt.lokalizacja)
+    label_imie_wartosc.config(text=imie)
+    label_wiek_wartosc.config(text=wiek)
+    label_lokalizacja_wartosc.config(text=lokalizacja)
 
     if aktualny_typ == "klient":
-        label_opis_wartosc.config(text=obiekt.opis)
+        label_opis_wartosc.config(text=aktualna_lista[i].opis)
     else:
-        label_opis_wartosc.config(text=obiekt.stanowisko)
+        label_opis_wartosc.config(text=aktualna_lista[i].stanowisko)
+
+    map_widget.set_position(aktualna_lista[i].coordinates[0], aktualna_lista[i].coordinates[1])
+    map_widget.set_zoom(12)
 
 def dodaj_randke():
     klient_randka = entry_klient_randka.get()
@@ -153,3 +177,11 @@ def pokaz_randki():
     listbox_randki.delete(0, END)
     for r in randki:
         listbox_randki.insert(END, f"{r.klient} ❤ {r.osoba} | {r.data}")
+
+def wyczysc_formularz():
+    entry_imie.delete(0, END)
+    entry_wiek.delete(0, END)
+    entry_opis.delete(0, END)
+    entry_lokalizacja.delete(0, END)
+
+    entry_imie.focus()
--- model.py
+++ model.py
@@ -1,9 +1,9 @@
-
 # -------------------- LISTY DANYCH --------------------
 klienci = []
 pracownicy = []
 randki = []
 
+#----------------DEFINIOWANIE KLAS----------------
 class klient:
     def __init__(self, imie, wiek, opis, lokalizacja, typ):
         self.imie = imie
@@ -12,12 +12,20 @@ class klient:
         self.lokalizacja = lokalizacja
         self.typ = typ
 
+        self.coordinates = [52.2297, 21.0122]
+        self.marker = None
+
+
 class pracownik:
     def __init__(self, imie, wiek, stanowisko, lokalizacja):
         self.imie = imie
         self.wiek = wiek
         self.stanowisko = stanowisko
         self.lokalizacja = lokalizacja
+
+        self.coordinates = [52.2297, 21.0122]
+        self.marker = None
+
 
 class randka:
     def __init__(self, klient, osoba, data):
